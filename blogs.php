ab105312273269694f31d836c871c12ded373e0c
Tim66262 <tim.damato@swisscom.com>
2017-11-09 15:33:14 +0100

    dfdf

--- blogs.php
+++ blogs.php
@@ -8,10 +8,6 @@
   // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe über alle Blogs und der Ausgabe mit PHP ersetzt werden
   $blogs = getUserNames();
   foreach($blogs as $blog){
-	  
-	  
-	  echo "<div><a href='index.php?function=blogs&bid=".$blog['uid']."' title='Blog auswählen'><h4>".$blog['name']."</h4></a></div>";
+	  echo "<div><a href='index.php?function=entries_public&bid=".$blog['uid']."' title='Blog auswählen'><h4>".$blog['name']."</h4></a></div>";
   }
 ?>
-
-
