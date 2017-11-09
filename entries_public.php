272077c3ff1ac7906093b5a4ed0c1aef43012e3a
blopen <nelson.lopez@bbcag.ch>
2017-11-09 16:02:22 +0100

    lol

--- entries_public.php
+++ entries_public.php
@@ -3,12 +3,14 @@
   // Hier Code... (Schlaufe über alle Einträge dieses Blogs)
 
   // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe über alle Blog-Beiträge und der Ausgabe mit PHP ersetzt werden
-  $entrys = getEntry(4);
-  foreach ($entrys as $entry) {
-    echo "<h4>".$entry['title']."</h4>";
+  //var_dump($_GET('bid'));
+  $entrys = getEntries($_GET['bid']);
+  //var_dump($entrys);
+ foreach ($entrys as $entry) {
+	 echo "<div>";
+   echo "<h4>".$entry["title"]."</h4>";
+   echo  $entry["content"]."</div>";
+	//var_dump($entry);
   }
 ?>
-  <div>
-  <h4>Hipster Ipsum, 01.11.2016 16:42:12</h4>
-  Neutra truffaut blog, 90's microdosing gochujang fingerstache helvetica etsy. Shoreditch fashion axe tote bag wayfarers normcore, freegan hot chicken sriracha 8-bit brunch actually live-edge quinoa. Trust fund sustainable forage tilde, etsy gentrify 8-bit poutine blog swag lomo pug. Truffaut ugh pinterest, umami tofu hoodie cronut. Crucifix skateboard single-origin coffee, vape slow-carb pork belly direct trade everyday carry photo booth schlitz venmo franzen. Air plant viral stumptown pabst disrupt. Readymade mumblecore tumeric kitsch hashtag, godard trust fund.
-  </div>
+
