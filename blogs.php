79787ee93849bff8f56ec0ccf6ddc31b3fb4b1ad
blopen <nelson.lopez@bbcag.ch>
2017-11-20 13:42:52 +0100

    ok

---
+++ blogs.php
@@ -0,0 +1,13 @@
+<?php
+  // Alle Blogs bzw. Benutzernamen holen und falls Blog bereits ausgewählt, entsprechenden Namen markieren
+  // Hier Code....
+
+  // Schlaufe über alle Blogs bzw. Benutzer
+  // Hier Code....
+
+  // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe über alle Blogs und der Ausgabe mit PHP ersetzt werden
+  $blogs = getUserNames();
+  foreach($blogs as $blog){
+	  echo "<div><a href='index.php?function=entries_public&bid=".$blog['uid']."' title='Blog auswählen'><h4>".$blog['name']."</h4></a></div>";
+  }
+?>
